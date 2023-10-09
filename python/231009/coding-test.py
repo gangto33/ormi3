86d2bdf298f7baa37db8fb220cd07476c8c58668
gangto33 <142385671+gangto33@users.noreply.github.com>
2023-10-09 23:41:51 +0900

    Add files via upload

--- python/231009/coding-test.py
+++ python/231009/coding-test.py
@@ -1,20 +1,17 @@
-# 코딩테스트 연습 - 연습문제 - 할인 행사
+# 코딩테스트 연습 - 월간 코드 챌린지 시즌2 - 괄호 회전하기
 
-def solution(want, number, discount):
+def solution(s):
     answer = 0
-    want_list = []
-    for i in range(len(want)):
-        want_list += [want[i]] * number[i]
-    
-    for i in range(len(discount)):
-        if i+sum(number) <= len(discount):
-            a = discount[i:i+sum(number)]
-            for x in want_list:
-                if x in a:
-                    a.remove(x)
-                else:
-                    break
-            if not a:
-                answer += 1
-                
+    correct = [['(', ')'], ['[', ']'], ['{', '}']]
+    stack = []
+    s = list(s)
+    for i in range(len(s)):
+        for x in s:
+            stack += [x]
+            if stack[-2:] in correct:
+                del stack[-2:]
+        if len(stack) == 0:
+            answer += 1
+        stack.clear()
+        s.append(s.pop(0))
     return answer
